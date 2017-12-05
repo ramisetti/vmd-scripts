aec75a21afbfcd6ddade3771d02c19e6aa73e9ce
S.B.Ramisetti <ramisettisrinivas@yahoo.com>
2017-12-05 11:56:40 +0000

    update script

--- com.tcl
+++ com.tcl
@@ -1,17 +1,28 @@
-set sel1 [atomselect top "resname CAS or resname C3S"] 
-set sel2 [atomselect top "resname CAT or resname C3T"]
+proc com {args} {
+    set usage {com "selection1 selection2" 
+Example: com "resname CAT or resname CAS" "resname C3T or resname C3S"
+comdist.txt is the output file}
+    if {[llength $args] < 2} {
+        error "wrong # args: should be $usage"
+    }
+    set arg1 [lindex $args 0]
+    set arg2 [lindex $args 1]
 
-set nf [molinfo top get numframes]
+    set sel1 [atomselect top "$arg1"] 
+    set sel2 [atomselect top "$arg2"]
 
-set outfile [open "comdist.txt" w]
+    set nf [molinfo top get numframes]
+    set outfile [open "comdist.txt" w]
 
-for {set i 0} {$i < $nf} {incr i} { 
-    puts "frame $i of $nf" 
-    $sel1 frame $i
-    $sel2 frame $i 
-    set com1 [measure center $sel1 weight mass]
-    set com2 [measure center $sel2 weight mass]
-    set simdata($i.r) [veclength [vecsub $com1 $com2]]
-    puts $outfile "$i $simdata($i.r) $com1 $com2"
+    puts $outfile "#frameNo distance com1_x com1_y com1_z com2_x com2_y com2_z"
+    for {set i 0} {$i < $nf} {incr i} { 
+	puts "frame $i of $nf" 
+	$sel1 frame $i
+	$sel2 frame $i 
+	set com1 [measure center $sel1 weight mass]
+	set com2 [measure center $sel2 weight mass]
+	set simdata($i.r) [veclength [vecsub $com1 $com2]]
+	puts $outfile "$i $simdata($i.r) $com1 $com2"
+    }
+    close $outfile
 }
-close $outfile
